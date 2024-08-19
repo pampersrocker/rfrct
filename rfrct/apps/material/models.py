97402a1dd8e85a1f446812862b630a12fc20f7a8
Marvin Pohl <marvin@lab132.com>
2024-08-19 22:33:48 +0200

    feat: Add Material model and views for creating laser materials

--- rfrct/apps/material/models.py
+++ rfrct/apps/material/models.py
@@ -1,2 +1,37 @@
 from extensions import nosql
+import datetime
 
+class MaterialType(nosql.Document):
+    name = nosql.StringField(max_length=255, required=True, unique=True)
+    description = nosql.StringField(max_length=255, required=True)
+    created_at = nosql.DateTimeField(default=datetime.datetime.now)
+    updated_at = nosql.DateTimeField(default=datetime.datetime.now)
+
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+    meta = {
+        'ordering': ['name']
+    }
+
+class Material(nosql.Document):
+    name = nosql.StringField(max_length=255, required=True, unique=True)
+    description = nosql.StringField(max_length=255, required=True)
+    #material_type = nosql.ReferenceField(MaterialType)
+    created_at = nosql.DateTimeField(default=datetime.datetime.now)
+    updated_at = nosql.DateTimeField(default=datetime.datetime.now)
+    price_per_square_meter = nosql.DecimalField(min_value=0, required=True)
+    thickness = nosql.DecimalField(min_value=0, required=True)
+
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+    meta = {
+        'ordering': ['name']
+    }
--- rfrct/apps/material/views.py
+++ rfrct/apps/material/views.py
@@ -1,10 +1,23 @@
 # keep this file even if you don't need traditional views
 # as it holds the blueprint app instance
 from flask import Blueprint
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from flask_mongoengine.wtf import model_form
+from .models import Material
 
 app = Blueprint('material', __name__, template_folder='templates')
 
 @app.route('/')
 def index():
     return render_template('material/index.html')
+
+@app.route('/create')
+def create():
+    form = model_form(Material)()
+    if form.validate_on_submit():
+        material = Material()
+        form.populate_obj(material)
+        material.save()
+        flash("Material created successfully", "success")
+        return redirect(url_for('material.index'))
+    return render_template('material/create.html', form=form)
